edf2045e5343e7605d489be6092663f1e07ab739
Chris Wilson <chrisw@myself.com>
2023-06-04 19:09:57 -0400

    mostly there. Routes done, working on seeds.

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  addReaction,
+  deleteThought,
+  updateThought,
+  removeReaction,
+} = require("../../controllers/thoughtController");
+
+router.route("/").get(getAllThoughts).post(createThought);
+
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
---
+++ seeds/userSeeds.js
@@ -0,0 +1,26 @@
+const { User } = require("../models");
+
+const userData = [
+  {
+    username: "MrWilson84",
+    email: "mrwilson@gmail.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "JazzyJ",
+    email: "JJ@hotmail.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "BFinny",
+    email: "bfinn@yahoo.com",
+    thoughts: [],
+    friends: [],
+  },
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
