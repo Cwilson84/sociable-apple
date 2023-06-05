5f7d4573a6f9cc43064e44d2cc605385b35caab6
Chris Wilson <chrisw@myself.com>
2023-06-05 17:19:18 -0400

    fixed server.js file

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require("express");
+const db = require("./config/connection");
 const routes = require("./routes");
-const app = express();
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-// middleware
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// routes
+app.use(express.json());
 app.use(routes);
 
-
-// mongoose connection
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
