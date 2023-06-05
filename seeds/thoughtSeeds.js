8358c2f643de9850d1175a2d037496a5a4bd380b
Chris Wilson <chrisw@myself.com>
2023-06-04 21:16:11 -0400

    still working on seeding

--- seeds/thoughtSeeds.js
+++ seeds/thoughtSeeds.js
@@ -2,18 +2,18 @@ const { Thought } = require("../models");
 
 const thoughtData = [
   {
-    thoughts: ["I love the new season of The Mandalorian!", "I'm so excited to master REACT!"],
+    thoughtText: ["I love the new season of The Mandalorian!", "I'm so excited to master REACT!"],
     username: "MrWilson84",
     reactions: ["I haven't watched it yet..."],
   },
   {
-    thoughts:[
+    thoughtText:[
       "Coding is like eating an elephant. You have to do it one bite at a time."],
     username: "BFinny",
     reactions: ["I like elephants!"],
   },
   {
-    thoughts: ["Who even sleeps anymore?"],
+    thoughtText: ["Who even sleeps anymore?"],
     username: "JazzyJ",
     reactions: ["I do!", "Often?", "Never!", "The weak! I'm a machine!"],
   }
