e9bb2c92a45613afd0b55a5451d4890d0a4f744c
Chris Wilson <chrisw@myself.com>
2023-06-04 21:22:17 -0400

    connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/sociable-apple";
+  process.env.MONGODB_URI || "PORT 3001";
 
 mongoose.connect(MONGODB_URI);
 
