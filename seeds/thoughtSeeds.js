4d4e7183c39c8538872c29ab7219b9c42ce03c22
Chris Wilson <chrisw@myself.com>
2023-06-04 20:53:51 -0400

    reorganized seed data

--- seeds/thoughtSeeds.js
+++ seeds/thoughtSeeds.js
@@ -2,25 +2,21 @@ const { Thought } = require("../models");
 
 const thoughtData = [
   {
-    thoughtText: "I love the new season of The Mandalorian!",
+    thoughts: ["I love the new season of The Mandalorian!", "I'm so excited to master REACT!"],
     username: "MrWilson84",
     reactions: ["I haven't watched it yet..."],
   },
   {
-    thoughtText:
-      "Coding is like eating an elephant. You have to do it one bite at a time.",
+    thoughts:[
+      "Coding is like eating an elephant. You have to do it one bite at a time."],
     username: "BFinny",
     reactions: ["I like elephants!"],
   },
   {
-    thoughtText: "Who even sleeps anymore?",
+    thoughts: ["Who even sleeps anymore?"],
     username: "JazzyJ",
     reactions: ["I do!", "Often?", "Never!", "The weak! I'm a machine!"],
-  },
-  {
-    thoughtText: "I'm so excited to learn about the MERN stack!",
-    username: "MrWilson84",
-  },
+  }
 ];
 
 const seedThoughts = () => Thought.insertMany(thoughtData);
