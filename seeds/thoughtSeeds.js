df3086d14e7d11d5f79a3052d646b1f125233f86
Chris Wilson <chrisw@myself.com>
2023-06-04 20:42:51 -0400

    fixed seed reactions to string

--- seeds/thoughtSeeds.js
+++ seeds/thoughtSeeds.js
@@ -4,18 +4,18 @@ const thoughtData = [
   {
     thoughtText: "I love the new season of The Mandalorian!",
     username: "MrWilson84",
-    reactions: ["😍", "I haven't watched it yet...", "😍"],
+    reactions: ["I haven't watched it yet..."],
   },
   {
     thoughtText:
       "Coding is like eating an elephant. You have to do it one bite at a time.",
     username: "BFinny",
-    reactions: ["😂", "😂", "😂", "😂"],
+    reactions: ["I like elephants!"],
   },
   {
     thoughtText: "Who even sleeps anymore?",
     username: "JazzyJ",
-    reactions: ["😴", "😂", "😂"],
+    reactions: ["I do!", "Often?", "Never!", "The weak! I'm a machine!"],
   },
   {
     thoughtText: "I'm so excited to learn about the MERN stack!",
